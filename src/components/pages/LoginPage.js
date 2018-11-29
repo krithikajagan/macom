37634164a2e6e5098ec30f1f7d000a97ed82be4f
Krithika Jagannathan <Krithika.Jagannathan@csscorp.com>
2018-11-29 16:59:08 +0530

    Initial Macom commit

---
+++ src/components/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import LoginForm from '../forms/LoginForm';
+const LoginPage = () => (
+  <div>
+    <h1>Login Page</h1>
+    <img src="../../../macom.jpeg" alt="Image not found!!" length ="100" width="100"/>
+    <LoginForm />
+  </div>);
+
+export default LoginPage;
